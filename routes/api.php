af88279dd471f57d20bf9730f94691de93e733d9
vichet <kuengvichet@mcnc.co.kr>
2024-06-24 17:24:20 +0700

    Modify api

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,6 @@ Route::post('/students', [StudentController::class, 'store']);
 Route::get('/students/{id}', [StudentController::class, 'show']);
 Route::put('/students/{id}', [StudentController::class, 'update']);
 Route::delete('/students/{id}', [StudentController::class, 'destroy']);
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
